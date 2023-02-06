f1f64dd5523f1d0e6185467c512f82c42dcf9e94
jordshub <119757179+jordshub@users.noreply.github.com>
2023-02-06 18:39:57 +0000

    files added

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+
+var geoReturn = "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName + "&appid=" + "8ef1c1d896dd98c2c59b6d511a0151cd";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + "lat=" +
+    returnedLat + "&lon=" + returnedLon + "&appid=" + "8ef1c1d896dd98c2c59b6d511a0151cd";
